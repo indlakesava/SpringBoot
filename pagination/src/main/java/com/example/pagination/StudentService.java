7465ac8ff7f45d7e271aed315d28b6970e111ee1
indlakesava <50535274+indlakesava@users.noreply.github.com>
2024-10-26 17:32:16 -0400

    Adding Pageable Interface

--- pagination/src/main/java/com/example/pagination/StudentService.java
+++ pagination/src/main/java/com/example/pagination/StudentService.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -37,10 +38,12 @@ Random random = new Random();
     }
 
     public Page findAllStudentsByPagination(int offset, int size) {
-        return repository.findAll(PageRequest.of(offset, size));
+        Pageable pageable = PageRequest.of(offset, size);
+        return repository.findAll(pageable);
     }
 
     public Page findAllStudentsBySortAndPagination(int offset, int size, String field) {
-        return repository.findAll(PageRequest.of(offset, size).withSort(Sort.by(field)));
+        Pageable pageable = PageRequest.of(offset, size).withSort(Sort.by(field));
+        return repository.findAll(pageable);
     }
 }
